4ae9d313d0bc3d1c6438b1811e960b54e1003eff
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-01-24 23:36:12 -0800

    layout restructuring of html

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 3000;
 
 var app = express();
+
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for app from "public" directory
 app.use(express.static('public'));
