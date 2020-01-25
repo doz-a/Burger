b3a9c990e75cfd3a564d83460284f41e4f7f7d1f
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-01-25 09:58:36 -0800

    connected to heroku jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // MySQL connection 
-var mysql = require('mysql');
+var mysql = require('mysql2');
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "5weetswisher",
+        database: "burgers_db"
+    });
+}
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "5weetswisher",
-    database: "burgers_db"
-});
+
 
 // Connection 
 connection.connect(function (err) {
