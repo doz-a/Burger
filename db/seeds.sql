01520db1268845e3bb018e9c9d0faea45b259adf
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-01-25 09:39:55 -0800

    updated seeds see if it works with heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,25 @@
 INSERT INTO burgers
     (burger_name)
 VALUES
-    ('Bacon Deluxe Cheese Steak Burger'),
-    ('Vegan Tomato Lettuce Burger'),
-    ('Whisky Fried Buffalo Barbecue Burger')
+    ('Bacon Deluxe Cheese Steak Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Vegan Tomato Lettuce Burger');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Whisky Fried Buffalo Barbecue Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chicken Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Fried Tuna Burger', true);
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Sad Veggie Burger');
+
